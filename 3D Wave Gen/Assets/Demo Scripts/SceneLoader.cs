1ccd4fc9d3006c18cd1201f46fac49a2b52384c6
Ben Windley <ben.windley@yahoo.co.uk>
2019-04-11 05:47:20 +0100

    CTP Submission commit

---
+++ 3D Wave Gen/Assets/Demo Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public int current_scene = 0;
+
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.E))
+        {
+            SceneManager.LoadScene(current_scene);
+        }
+	}
+}
---
+++ 3D Wave Gen/Assets/Editor/MeshReloadEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MeshGenerator))]
+public class MeshReloadEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MeshGenerator mesh_generator = (MeshGenerator)target;
+        if(GUILayout.Button("Reload Mesh"))
+        {
+            mesh_generator.UpdateMesh();
+        }
+    }
+}
